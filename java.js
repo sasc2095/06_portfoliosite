8fd2ac44966ddc6fa0bf389b02abc5181ff11377
Sascha Nielsen <sasc2095@stud.kea.dk>
2022-06-04 18:36:43 +0200

    burger menu + dropdown

---
+++ java.js
@@ -0,0 +1,13 @@
+//kryds//
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
